c20c4daed00c58168dd3e88edbabe062ec2a7ad9
AndresFelipe21 <f.castellanos2109@gmail.com>
2024-05-15 22:04:42 -0500

    memoria
    se genera arreglo del juego en formato xhtml

--- Funny_Mind_JSF/target/generated-sources/annotations/com/mycompany/funny_mind_jsf/entidades/Ejercicios_.java
+++ Funny_Mind_JSF/target/generated-sources/annotations/com/mycompany/funny_mind_jsf/entidades/Ejercicios_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2024-05-15T20:20:43")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2024-05-15T21:28:44")
 @StaticMetamodel(Ejercicios.class)
 public class Ejercicios_ { 
 
--- Funny_Mind_JSF/target/generated-sources/annotations/com/mycompany/funny_mind_jsf/entidades/Terapeutas_.java
+++ Funny_Mind_JSF/target/generated-sources/annotations/com/mycompany/funny_mind_jsf/entidades/Terapeutas_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2024-05-15T20:20:43")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2024-05-15T21:28:44")
 @StaticMetamodel(Terapeutas.class)
 public class Terapeutas_ { 
 
