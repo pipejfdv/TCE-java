6d5cf130f3f46818853a4bc59e3b4c7b34f8aa8e
AndresFelipe21 <f.castellanos2109@gmail.com>
2024-05-15 19:47:15 -0500

    categorias
    se genera el cambio a formato xhtml

--- Funny_Mind_JSF/target/generated-sources/annotations/com/mycompany/funny_mind_jsf/entidades/Pacientes_.java
+++ Funny_Mind_JSF/target/generated-sources/annotations/com/mycompany/funny_mind_jsf/entidades/Pacientes_.java
@@ -10,7 +10,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2024-05-15T17:46:23")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2024-05-15T19:10:11")
 @StaticMetamodel(Pacientes.class)
 public class Pacientes_ { 
 
--- Funny_Mind_JSF/target/generated-sources/annotations/com/mycompany/funny_mind_jsf/entidades/Usuarios_.java
+++ Funny_Mind_JSF/target/generated-sources/annotations/com/mycompany/funny_mind_jsf/entidades/Usuarios_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2024-05-15T17:46:23")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2024-05-15T19:10:11")
 @StaticMetamodel(Usuarios.class)
 public class Usuarios_ { 
 
