64359fd3a68e2f8757a8967cd5ed426ba916bf9b
Juan Felipe Delgadillo V <89826767+pipejfdv@users.noreply.github.com>
2024-05-18 18:02:22 -0500

    Update TCE BD
    se crearon las tablas puntaje y avance, se elimina la de gravedad

--- funnymind/TCE BD.sql
+++ funnymind/TCE BD.sql
@@ -1,187 +1,105 @@
-CREATE DATABASE tce;
-USE tce;
--- tabla de perfil usuario
-CREATE TABLE usuarios (
- id INT NOT NULL AUTO_INCREMENT,
- primer_nombre VARCHAR(50) NOT NULL,
- segundo_nombre VARCHAR(50) NULL,
- primer_apellido VARCHAR(50) NOT NULL,
- segundo_apellido VARCHAR(50) NOT,
- fecha_nacimiento DATE NOT NULL,
- genero ENUM('Masculino', 'Femenino') NOT NULL,
- correo VARCHAR(100) NOT NULL,
- contrasena VARCHAR(255) NOT NULL,
- fecha_registro TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
- PRIMARY KEY (id)
-);
--- tabla de perfil terapeuta
-CREATE TABLE perfil_terapeuta (
- id INT NOT NULL AUTO_INCREMENT,
- primer_nombre VARCHAR(50) NOT NULL,
- segundo_nombre VARCHAR(50) NULL,
- primer_apellido VARCHAR(50) NOT NULL,
- segundo_apellido VARCHAR(50) NOT,
- espcialidad_id int null,
- fecha_nacimiento DATE NOT NULL,
- genero ENUM('Masculino', 'Femenino') NOT NULL,
- correo VARCHAR(100) NOT NULL,
- contrasena VARCHAR(255) NOT NULL,
- fecha_registro TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
- PRIMARY KEY (id)
-);
-
--- identificar el tipo de gravedad
-CREATE TABLE gravedad (
- id INT NOT NULL AUTO_INCREMENT,
- usuario_id INT NOT NULL,
- fecha_diagnostico DATE NOT NULL,
- tipo_trauma ENUM('Traumatismo craneoencefálico leve', 'Traumatismo craneoencefálico moderado', 'Traumatismo craneoencefálico grave') NOT NULL,
- fecha_registro TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
- PRIMARY KEY (id),
- FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
-);
-
-CREATE TABLE ejercicios (
- id INT NOT NULL AUTO_INCREMENT,
- nombre VARCHAR(50) NOT NULL,
- descripcion TEXT,
- fecha_creacion DATE NOT NULL,
- fecha_modificacion TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
- PRIMARY KEY (id)
-);
-
-CREATE TABLE ejercicios_asignados (
- id INT NOT NULL AUTO_INCREMENT,
- gravedad_id INT NOT NULL,
- ejercicio_id INT NOT NULL,
- fecha_asignacion DATE NOT NULL,
- PRIMARY KEY (id),
- FOREIGN KEY (gravedad_id) REFERENCES gravedad(id),
- FOREIGN KEY (ejercicio_id) REFERENCES ejercicios(id)
-);
-
-CREATE TABLE especialidad (
- PRIMARY KEY (id),
- id INT NOT NULL,
- nombre VARCHAR(50),
- terapeuta_id int NOT NULL,
- FOREIGN KEY (terapeuta_id) REFERENCES perfil_terapeuta (terapeuta_id)
-);
-********************************************************PENDIENTE DE AQUI EN ADELANTE
-CREATE TABLE sesiones (
- id INT NOT NULL AUTO_INCREMENT,
- paciente_id INT NOT NULL,
- terapeuta_id INT NOT NULL,
- fecha_sesion DATETIME NOT NULL,
- duracion INT NOT NULL,
- observaciones TEXT,
- PRIMARY KEY (id),
- FOREIGN KEY (paciente_id) REFERENCES pacientes(id),
- FOREIGN KEY (terapeuta_id) REFERENCES terapeutas(id)
-);
-******************************************************************************************
-
+-- Eliminar la base de datos existente si existe y crear una nueva
+DROP DATABASE IF EXISTS tce;
 CREATE DATABASE tce;
 USE tce;
 
--- tabla de usuarios
+-- Tabla de usuarios
 CREATE TABLE usuarios (
- id INT NOT NULL AUTO_INCREMENT,
- nombre VARCHAR(50) NOT NULL,
- apellido VARCHAR(50) NOT NULL,
- fecha_nacimiento DATE NOT NULL,
- genero ENUM('Masculino', 'Femenino') NOT NULL,
- correo VARCHAR(100) NOT NULL,
- contrasena VARCHAR(255) NOT NULL,
- rol ENUM('Administrador', 'Terapeuta', 'Paciente') NOT NULL,
- fecha_registro TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
- afiliado_eps TINYINT NOT NULL DEFAULT 0,
- PRIMARY KEY (id)
-);
-
--- tabla de pacientes
-CREATE TABLE pacientes (
- id INT NOT NULL AUTO_INCREMENT,
- usuario_id INT NOT NULL,
- fecha_diagnostico DATE NOT NULL,
- tipo_trauma ENUM('Traumatismo craneoencefálico leve', 'Traumatismo craneoencefálico moderado', 'Traumatismo craneoencefálico grave') NOT NULL,
- fecha_registro TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
- PRIMARY KEY (id),
- FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
-);
-
---  tabla de afiliaciones_eps
-CREATE TABLE afiliaciones_eps (
- id INT NOT NULL AUTO_INCREMENT,
- paciente_id INT NOT NULL,
- eps_afiliada VARCHAR(100) NOT NULL,
- fecha_afiliacion DATE NOT NULL,
- activo TINYINT NOT NULL DEFAULT 0,
- fecha_registro TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
- PRIMARY KEY (id),
- FOREIGN KEY (paciente_id) REFERENCES pacientes(id)
-);
-
---  tabla de ejercicios
-CREATE TABLE ejercicios (
- id INT NOT NULL AUTO_INCREMENT,
- nombre VARCHAR(50) NOT NULL,
- descripcion TEXT,
- fecha_creacion DATE NOT NULL,
- fecha_modificacion TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
- PRIMARY KEY (id)
-);
-
---  tabla de ejercicios_asignados
-CREATE TABLE ejercicios_asignados (
- id INT NOT NULL AUTO_INCREMENT,
- paciente_id INT NOT NULL,
- ejercicio_id INT NOT NULL,
- fecha_asignacion DATE NOT NULL,
- PRIMARY KEY (id),
- FOREIGN KEY (paciente_id) REFERENCES pacientes(id),
- FOREIGN KEY (ejercicio_id) REFERENCES ejercicios(id)
-);
-
---  tabla de terapeutas
-CREATE TABLE terapeutas (
- id INT NOT NULL AUTO_INCREMENT,
- usuario_id INT NOT NULL,
- especialidad VARCHAR(50) NOT NULL,
- fecha_registro TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
- PRIMARY KEY (id),
- FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
-);
-
--- tabla de especialidad
-CREATE TABLE especialidad (
- id INT NOT NULL,
- nombre VARCHAR(50),
- PRIMARY KEY (id)
-);
-
--- tabla de sesiones
-CREATE TABLE sesiones (
- id INT NOT NULL AUTO_INCREMENT,
- paciente_id INT NOT NULL,
- terapeuta_id INT NOT NULL,
- fecha_sesion DATETIME NOT NULL,
- duracion INT NOT NULL,
- observaciones TEXT,
- PRIMARY KEY (id),
- FOREIGN KEY (paciente_id) REFERENCES pacientes(id),
- FOREIGN KEY (terapeuta_id) REFERENCES terapeutas(id)
-);
-
--- tabla de puntajes_actividades
-CREATE TABLE puntajes_actividades (
     id INT NOT NULL AUTO_INCREMENT,
-    paciente_id INT NOT NULL,
-    actividad_id INT NOT NULL,
-    fecha_realizacion DATE NOT NULL,
-    puntaje INT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (paciente_id) REFERENCES pacientes(id),
-    FOREIGN KEY (actividad_id) REFERENCES ejercicios(id)
+    primer_nombre VARCHAR(50) NOT NULL,
+    segundo_nombre VARCHAR(50) NULL,
+    primer_apellido VARCHAR(50) NOT NULL,
+    segundo_apellido VARCHAR(50) NULL,
+    fecha_nacimiento DATE NOT NULL,
+    genero ENUM('Masculino', 'Femenino') NOT NULL,
+    correo VARCHAR(100) NOT NULL,
+    contrasena VARCHAR(255) NOT NULL,
+    fecha_registro TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
 );
 
+-- Tabla de perfil terapeuta
+CREATE TABLE perfil_terapeuta (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    primer_nombre VARCHAR(50) NOT NULL,
+    segundo_nombre VARCHAR(50) NULL,
+    primer_apellido VARCHAR(50) NOT NULL,
+    segundo_apellido VARCHAR(50) NULL,
+    especialidad_id INT NOT NULL,
+    fecha_nacimiento DATE,
+    genero ENUM('Masculino', 'Femenino') NOT NULL,
+    correo VARCHAR(100) NOT NULL,
+    contrasena VARCHAR(255) NOT NULL,
+    fecha_registro TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Tabla de especialidad del doctor
+CREATE TABLE especialidad (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50),
+    terapeuta_id INT NOT NULL,
+    FOREIGN KEY (terapeuta_id) REFERENCES perfil_terapeuta(id)
+);
+
+-- Tabla de juegos
+CREATE TABLE juegos (
+    id INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    descripcion TEXT,
+    fecha_creacion DATE NOT NULL,
+    fecha_modificacion TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+
+-- Tabla de categoría de juegos
+CREATE TABLE categoria_juegos (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    categoria ENUM ('Memoria', 'Atención', 'Lógica'),
+    juegos_id INT NOT NULL,
+    FOREIGN KEY (juegos_id) REFERENCES juegos(id)
+);
+
+-- Tabla de juegos asignados
+CREATE TABLE juegos_asignados (
+    id INT NOT NULL AUTO_INCREMENT,
+    gravedad_id INT NOT NULL,
+    juegos_id INT NOT NULL,
+    fecha_asignacion DATE NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (gravedad_id) REFERENCES gravedad(id),
+    FOREIGN KEY (juegos_id) REFERENCES juegos(id)
+);
+
+-- Tabla de avance
+CREATE TABLE avance (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    descripcion VARCHAR(255),
+    usuario_id INT,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+);
+
+-- Tabla de sesiones
+CREATE TABLE sesiones (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    usuario_id INT NOT NULL,
+    perfil_terapeuta_id INT NOT NULL,
+    fecha_sesion DATETIME NOT NULL,
+    duracion INT NOT NULL,
+    avance_id INT,
+    FOREIGN KEY (avance_id) REFERENCES avance(id),
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
+    FOREIGN KEY (perfil_terapeuta_id) REFERENCES perfil_terapeuta(id)
+);
+
+-- Tabla de puntajes de actividades
+CREATE TABLE puntajes_actividades (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    usuario_id INT NOT NULL,
+    juegos_id INT NOT NULL,
+    categoria_id INT NOT NULL,
+    fecha_realizacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    puntaje INT NULL,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
+    FOREIGN KEY (categoria_id) REFERENCES categoria_juegos(id),
+    FOREIGN KEY (juegos_id) REFERENCES juegos(id)
+);
